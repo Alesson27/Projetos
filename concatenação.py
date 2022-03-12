4c34d9df67fbb5315950057671c366b4d2fdf38d
Alesson27 <101441147+Alesson27@users.noreply.github.com>
2022-03-12 00:40:30 -0300

    Create concatenação.py
    Código de estudos iniciais no Python.

---
+++ concatenação.py
@@ -0,0 +1,14 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name +" "+ last_name
+print(full_name.title())
+first_name = "alesson"
+second_name = "alves"
+last_name = "de araújo"
+full_name = first_name +" "+ second_name + last_name
+print("Hello, " + full_name.title())
+first_name = "morgana"
+last_name = "resende"
+full_name = first_name +" "+ last_name
+print("Hello, " + full_name.title() + "!")
+
