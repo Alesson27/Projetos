34d9e46112c07a09cd86339a8876792d80b909c5
Alesson27 <101441147+Alesson27@users.noreply.github.com>
2022-03-12 00:55:53 -0300

    Update concatenação.py

--- concatenação.py
+++ concatenação.py
@@ -1,14 +1,20 @@
 first_name = "ada"
 last_name = "lovelace"
-full_name = first_name +" "+ last_name
+full_name = first_name + " " + last_name
 print(full_name.title())
+
 first_name = "alesson"
 second_name = "alves"
 last_name = "de araújo"
-full_name = first_name +" "+ second_name + last_name
+full_name = first_name + " " + second_name + last_name
 print("Hello, " + full_name.title())
+
 first_name = "morgana"
 last_name = "resende"
-full_name = first_name +" "+ last_name
+full_name = first_name + " " + last_name
 print("Hello, " + full_name.title() + "!")
 
+name_1 = 'alesson'
+name_2 = 'alves'
+full_name = name_1 + " " + name_2
+print("Hello, " + full_name.title() + "!")
